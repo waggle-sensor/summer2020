28972ae89c9a767fcfea3e535f11e090dd8e1e95
Spencer Ng <sng1488@gmail.com>
2020-06-25 12:03:39 -0400

    Add stratified sampling, class filters for training

--- ng/char-cleanup/char_cleanup.py
+++ ng/char-cleanup/char_cleanup.py
@@ -4,60 +4,103 @@ import os
 import glob
 import random
 
-DATA = "./letters/"
+DATA = "./data/"
 OUTPUT = "./output/"
+NUM_CLASSES = 8
 
 
-def generate_classes(out_file):
+def generate_all_classes():
     classes = [str(i) for i in range(10)]
     for c in string.ascii_uppercase:
         classes.append(c)
     for c in string.ascii_lowercase:
         classes.append(c)
 
-    with open(out_file, "w+") as out:
-        out.write("\n".join(classes) + "\n")
+    return classes
 
 
-def create_labels(img_paths):
+def create_labels(img_paths, classes, all_classes):
     for path in img_paths:
+        orig_idx = int(path.split("Sample0")[1][:2]) - 1
+        if all_classes[orig_idx] not in classes:
+            continue
+
         txt_path = path.replace("images", "labels").replace(".png", ".txt")
 
         folder_path = "/".join(txt_path.split("/")[:-1])
         os.makedirs(folder_path, exist_ok=True)
 
-        idx = int(path.split("Sample0")[1][:2]) - 1
+        idx = classes.index(all_classes[orig_idx])
         line = f"{idx} 0.5 0.5 1 1"
 
         with open(txt_path, "w+") as out:
             out.write(line)
 
 
-def split_test_train(img_paths, prop_train):
-    random.shuffle(img_paths)
+def split_test_train(img_dict, prop_train):
+    train = open(OUTPUT + "train.txt", "w+")
+    test = open(OUTPUT + "test.txt", "w+")
 
-    train_num = int(prop_train * len(img_paths))
-    train_list = img_paths[:train_num]
-    test_list = img_paths[train_num : len(img_paths)]
+    for class_name, imgs in img_dict.items():
+        random.shuffle(imgs)
 
-    with open(OUTPUT + "train.txt", "w+") as out:
-        out.write("\n".join(train_list))
+        train_num = round(prop_train * len(imgs))
+        train_list = imgs[:train_num]
+        test_list = imgs[train_num : len(imgs)]
 
-    with open(OUTPUT + "test.txt", "w+") as out:
-        out.write("\n".join(test_list))
+        train.write("\n".join(train_list) + "\n")
+        test.write("\n".join(test_list) + "\n")
+
+    train.close()
+    test.close()
+
+def get_img_dict(img_paths, classes):
+    """Create a dictionary of images by class."""
+    img_dict = dict()
+
+    for c in classes:
+        img_dict[c] = list()
+    for path in img_paths:
+        idx = int(path.split("Sample0")[1][:2]) - 1
+        img_dict[classes[idx]].append(path)
+
+    return img_dict
+
+
+def filter_img_dict(img_dict, classes):
+    filtered_dict = dict()
+    for k, v in img_dict.items():
+        if k in classes:
+            filtered_dict[k] = v
+    return filtered_dict
 
 
 def main():
     os.makedirs(OUTPUT, exist_ok=True)
 
-    class_file = OUTPUT + "chars.names"
-    if not os.path.exists(class_file):
-        generate_classes(class_file)
-
     img_paths = glob.glob(DATA + "images/**/*.png", recursive=True)
 
-    create_labels(img_paths)
-    split_test_train(img_paths, 0.7)
+    all_classes = generate_all_classes()
+    img_dict = get_img_dict(img_paths, all_classes)
+
+    freq = {k: len(v) for k, v in img_dict.items()}
+    sorted_freq = {
+        k: v for k, v in sorted(freq.items(), key=lambda item: item[1], reverse=True)
+    }
+
+    with open(OUTPUT + "freq.txt", "w+") as out:
+        for k, v in sorted_freq.items():
+            out.write(f"{k}: {v}\n")
+
+    classes = list(sorted_freq.keys())[:NUM_CLASSES]
+
+    with open(OUTPUT + "chars.names", "w+") as out:
+        out.write("\n".join(classes) + "\n")
+
+    create_labels(img_paths, classes, all_classes)
+
+    filtered_dict = filter_img_dict(img_dict, classes)
+    split_test_train(filtered_dict, 0.75)
 
 
 if __name__ == "__main__":
