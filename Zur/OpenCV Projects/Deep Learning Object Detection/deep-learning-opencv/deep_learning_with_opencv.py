4614b42fef2816a71803e569800e07ea7d8c4c2a
Ori Zur <orizur@Oris-MacBook-Pro.local>
2020-07-10 16:04:23 -0700

    Zur 7/10

--- Zur/OpenCV Projects/Deep Learning Object Detection/deep-learning-opencv/deep_learning_with_opencv.py
+++ Zur/OpenCV Projects/Deep Learning Object Detection/deep-learning-opencv/deep_learning_with_opencv.py
@@ -1,5 +1,5 @@
-# USAGE
-# python deep_learning_with_opencv.py --image images/jemma.png --prototxt bvlc_googlenet.prototxt --model bvlc_googlenet.caffemodel --labels synset_words.txt
+# USAGE python deep_learning_with_opencv.py --image images/jemma.png --prototxt bvlc_googlenet.prototxt --model
+# bvlc_googlenet.caffemodel --labels synset_words.txt
 
 # import the necessary packages
 import numpy as np
@@ -10,13 +10,13 @@ import cv2
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
+                help="path to input image")
 ap.add_argument("-p", "--prototxt", required=True,
-	help="path to Caffe 'deploy' prototxt file")
+                help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", required=True,
-	help="path to Caffe pre-trained model")
+                help="path to Caffe pre-trained model")
 ap.add_argument("-l", "--labels", required=True,
-	help="path to ImageNet labels (i.e., syn-sets)")
+                help="path to ImageNet labels (i.e., syn-sets)")
 args = vars(ap.parse_args())
 
 # load the input image from disk
@@ -51,17 +51,14 @@ idxs = np.argsort(preds[0])[::-1][:5]
 
 # loop over the top-5 predictions and display them
 for (i, idx) in enumerate(idxs):
-	# draw the top prediction on the input image
-	if i == 0:
-		text = "Label: {}, {:.2f}%".format(classes[idx],
-			preds[0][idx] * 100)
-		cv2.putText(image, text, (5, 25),  cv2.FONT_HERSHEY_SIMPLEX,
-			0.7, (0, 0, 255), 2)
+    # draw the top prediction on the input image
+    if i == 0:
+        text = "Label: {}, {:.2f}%".format(classes[idx],preds[0][idx] * 100)
+        cv2.putText(image, text, (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-	# display the predicted label + associated probability to the
-	# console	
-	print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1,
-		classes[idx], preds[0][idx]))
+    # display the predicted label + associated probability to the
+    # console
+    print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1, classes[idx], preds[0][idx]))
 
 # display the output image
 cv2.imshow("Image", image)
