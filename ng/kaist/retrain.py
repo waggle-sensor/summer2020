5bf69a66e0b6ecf2ade20afb24f323b4ce9ff25d
Spencer Ng <sng1488@gmail.com>
2020-07-20 18:15:30 -0400

    Fix bugs in retrain script

--- ng/kaist/retrain.py
+++ ng/kaist/retrain.py
@@ -21,16 +21,19 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--epoch", type=int, help="benchmark epoch for retraining", default=None
+        "--epoch",
+        type=int,
+        help="benchmark epoch for retraining",
+        default=None,
+        required=True,
     )
     parser.add_argument("--from_file", type=str, help="load from file", default=None)
     opt = parser.parse_args()
 
-    if opt.epoch is None and opt.from_file is None:
-        print("Error: must specify a file or epoch to retrain from")
-        exit(0)
+    if opt.from_file is not None:
+        results, _ = utils.load_data(opt.from_file, by_actual=False)
 
-    if opt.epoch is not None:
+    else:
         if not os.path.exists(f"output/benchmark_{opt.epoch}.csv"):
             benchmark.benchmark(
                 "yolov3",
@@ -42,11 +45,8 @@ if __name__ == "__main__":
             )
 
         results, _ = utils.load_data(
-            f"output/benchmark_{check_num}.csv", by_actual=False
+            f"output/benchmark_{opt.epoch}.csv", by_actual=False
         )
-
-    else:
-        results, _ = utils.load_data(opt.from_file, by_actual=False)
 
     for name, func in methods.items():
         sample.create_sample(data_file, results, False, name, func)
@@ -58,12 +58,12 @@ if __name__ == "__main__":
         sp.run(aug_cmd, check=True)
 
         data_config = f"output/configs-retrain/{name}/chars.data"
-        bench_weights = f"checkpoints/yolov3_ckpt_{check_num}.pth"
+        bench_weights = f"checkpoints/yolov3_ckpt_{opt.epoch}.pth"
 
-        epoch_num = check_num + 26
+        epoch_num = opt.epoch + 26
         train_cmd = (
             f"python3 ../yolov3/train.py --epoch {epoch_num} --data_config {data_config} "
-            + f"--pretrained_weights {bench_weights} --img_size 416 --resume {check_num} "
+            + f"--pretrained_weights {bench_weights} --img_size 416 --resume {opt.epoch} "
             + f"--prefix {name} --clip 1.0 --batch_size 16"
         )
         train_cmd = train_cmd.split(" ")
