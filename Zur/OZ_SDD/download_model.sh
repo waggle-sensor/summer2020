3a8b68788a450113cc711848f095b838584cae86
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-08-14 15:33:44 -0500

    added coco.names. simplified download

--- Zur/OZ_SDD/download_model.sh
+++ Zur/OZ_SDD/download_model.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -e
 
 mkdir -p yolo-coco
-wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg -O yolo-coco/yolov3.cfg.download
-wget https://pjreddie.com/media/files/yolov3.weights -O yolo-coco/yolov3.weights.download
-
-# once *both* files have been successfully downloaded then rename
-mv yolo-coco/yolov3.cfg.download yolo-coco/yolov3.cfg
-mv yolo-coco/yolov3.weights.download yolo-coco/yolov3.weights
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names -O yolo-coco/coco.names
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg -O yolo-coco/yolov3.cfg
+wget https://pjreddie.com/media/files/yolov3.weights -O yolo-coco/yolov3.weights
